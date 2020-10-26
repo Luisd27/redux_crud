4dccea9278c368ed50896bdf87a1fc13380bdf64
Luis Juarez <jsluisd27@gmail.com>
2020-10-26 11:59:56 -0700

    Setup redux into app

--- crud/src/index.js
+++ crud/src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider, provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+      applyMiddleware(thunk)
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
